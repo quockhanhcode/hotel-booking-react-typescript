385fd9e5b3e8d92192793e222721956d978d589a
vuong pham <vuongpq@a-livesystems.com>
2025-11-06 12:57:57 +0700

    Fix layout userManagement && Set Store adminTemplate

---
+++ src/stores/adminTemplate.store.ts
@@ -0,0 +1,10 @@
+import { create } from 'zustand'
+
+export const useAdmin = create((set) => ({
+    tabSidebar: "overview",
+    setTabSidebar: (newTabSidebar: string) => set({ tabSidebar: newTabSidebar })
+    //   bears: 0,
+    //   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+    //   removeAllBears: () => set({ bears: 0 }),
+    //   updateBears: (newBears) => set({ bears: newBears }),
+}))
